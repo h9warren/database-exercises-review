001a479f996d0bbcdb68c6e8d4d832cda0fe7f06
Hunter Warren <hunter.warren@gmail.com>
2018-08-30 22:05:47 -0500

    first commit:

---
+++ WebContent/albums_migration.sql
@@ -0,0 +1,11 @@
+USE codeup_test_db;
+DROP TABLE IF EXISTS albums;
+CREATE TABLE IF NOT EXISTS codeup_test_db.albums (
+	id int(10) unsigned NOT NULL AUTO_INCREMENT,
+	artist varchar(50),
+	name VARCHAR(50),
+	release_date INT UNSIGNED,
+	sales DOUBLE(15,2),
+	genre VARCHAR(30),
+	PRIMARY KEY (ID)
+);
