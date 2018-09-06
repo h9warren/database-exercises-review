bfd426078e80619d38599d54048a2f7b414c46ab
Hunter Warren <hunter.warren@gmail.com>
2018-09-05 21:55:37 -0500

    creating seeder

---
+++ WebContent/albums_seeder.sql
@@ -0,0 +1,12 @@
+USE codeup_test_db;
+INSERT into albums (artist, name, release_date, genre, sales)
+VALUES 
+('Michael Jackson', 'Thriller', 1982, 'Pop, rock, R&B', 47.3),
+('Eagles', 'Their Greatest Hits (1971–1975)', 1976, 'Country rock, soft rock, folk rock', 41.2),
+('AC/DC', 'Back in Black', 1980, 'Hard rock', 26.1),
+('Pink Floyd', 'The Dark Side of the Moon', 1973, 'Progressive rock', 24.2),
+('Meat Loaf', 'Bat Out of Hell', 1977, 'Hard rock, progressive rock', 21.5),
+('Whitney Houston / Various artists', 'The Bodyguard', 1992, 'R&B', soul, pop, soundtrack, 28.4),
+('Eagles', 'Hotel California', 1976, 'Soft rock', 31.5),
+('Bee Gees / Various artists', 'Saturday Night Fever', 1977, 'Disco', 21.6),
+('Fleetwood Mac', 'Rumours', 1977, 'Soft rock', 27.9);
